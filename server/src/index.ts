4f346c6766da59063ddcb073cdd739e10f422bfe
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-10 01:35:34 +0530

    organized mikro-orm config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,10 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "lireddit",
-    user: "postgres",
-    password: "postgres",
-    type: "postgresql",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroOrmConfig);
 
   const post = orm.em.create(Post, { title: "give it a try" });
   await orm.em.persistAndFlush(post);
