3be0c786ca0cec7c7a48bee33d9486b568464704
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-11 01:12:15 +0530

    apollo server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,10 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
 
 const PORT = process.env.PORT || 4000;
 
@@ -10,7 +14,21 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
   app.get("/", (req, res) => res.status(200).send("API is running..."));
+
   app.listen(PORT, () =>
     console.log(`Server up and running at ${PORT} and http://localhost:4000`)
   );
