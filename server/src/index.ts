7bef8e7d0ca787926f13ba301b33e041d34b6e2f
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-10 20:02:27 +0530

    express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+
+const PORT = process.env.PORT || 4000;
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "give it a try brooo!!!" });
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const app = express();
+  app.get("/", (req, res) => res.status(200).send("API is running..."));
+  app.listen(PORT, () =>
+    console.log(`Server up and running at ${PORT} and http://localhost:4000`)
+  );
 };
 
 main().catch(console.error);
