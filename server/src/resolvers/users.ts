af7c86f8df34c0d73a6377f5afcf85989ffc686e
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-15 23:31:56 +0530

    added login resolver and renamed register resolver

--- server/src/resolvers/users.ts
+++ server/src/resolvers/users.ts
@@ -2,6 +2,9 @@ import { RequiredEntityData } from "@mikro-orm/core";
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
 import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import argon2 from "argon2";
+import { UsernamePasswordInput } from "./types/UsernamePasswordInput";
+import { UserResponse } from "./types/UserResponse";
 
 @Resolver()
 export class UserResolver {
@@ -11,16 +14,55 @@ export class UserResolver {
   }
 
   @Mutation(() => User)
-  async createUser(
-    @Arg("username", () => String) username: string,
-    @Arg("password", () => String) password: string,
+  async register(
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
   ): Promise<User> {
+    const { username, password: plainPassword } = options;
+    const hashedPassword = await argon2.hash(plainPassword);
     const user = em.create(User, {
       username,
-      password,
+      password: hashedPassword,
     } as RequiredEntityData<User>);
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options", () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const { username, password: plainPassword } = options;
+
+    const user = await em.findOne(User, { username });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: `that username doesn't exists`,
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, plainPassword);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: `that password doesn't match`,
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
